0e8edae903babf9aa1799b8921b0d07ae2a86ea3
bonvi83 <bonvi83@gmail.com>
2024-01-08 15:49:01 +0100

    forse ci siamo

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 // chiedo all'utente il suo nome
-const name = parseInt (prompt("Qualè il tuo nome?"))
+const name = prompt("Qualè il tuo nome?");
+console.log (name);
 
 // chiedo all'utente il suo cognnome
-const surname = prompt("Qualè il tuo cognome?")
+const surname = prompt("Qualè il tuo cognome?");
+console.log (surname);
 
 // chiedo all'utente il suo colore preferito
-const color = prompt("Qualè il tuo colore preferito?")
+const color = prompt("Qualè il tuo colore preferito?");
+console.log (color);
 
 // aggiungo alla password il numero 23
+let password = name + surname + color + "23";
+console.log (password);
