4ce008c66d123d187188c369751de5f65b38396f
bonvi83 <bonvi83@gmail.com>
2024-01-08 15:23:56 +0100

    add funzionalità (che non funzionano)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
 // chiedo all'utente il suo nome
-
-
+const name = prompt("Qualè il tuo nome?")
 
 // chiedo all'utente il suo cognnome
-
-
+const surname = prompt("Qualè il tuo cognome?")
 
 // chiedo all'utente il suo colore preferito
-
-
+const color = prompt("Qualè il tuo colore preferito?")
 
 // aggiungo alla password il numero 23
